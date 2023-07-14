5f63e789a20a17f306f23ae8af722a0bc5bfb0de
Cameron Hummels <chummels@gmail.com>
2023-07-14 13:22:37 -0700

    Fixing the zlims and improving things

--- make_healpy.py
+++ make_healpy.py
@@ -3,6 +3,7 @@ Code for converting Sam's theta-phi numpy arrays to a healpy image
 """
 import healpy as hp
 import numpy as np
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 import sys
 import os
@@ -19,21 +20,34 @@ name = os.path.splitext(os.path.basename(fn))[0]
 # Key to hydrogen column density or dispersion measure based on filename
 if name.startswith('NH'):
     mi = 1e18
+    ma = np.inf
     cbtext = 'log($N_{H}$ [$cm^{-2}$])'
+    cmap = mpl.cm.viridis
+#    cmap = mpl.cm.inferno
 if name.startswith('Ne'):
     mi = 1e0
+    ma = 1e3
     cbtext = 'log(DM [$pc \cdot cm^{-3}$])'
-ma = np.inf
-
+    cmap = mpl.cm.inferno
 
 # Automatically get the array size from the loaded numpy array
 struct = np.load(fn)
+print('Max = %g' % np.max(struct))
+print('Mean = %g' % np.mean(struct))
+print('Median = %g' % np.median(struct))
 n_theta = struct.shape[0]
 n_phi = struct.shape[1]
 theta = np.linspace(0, np.pi, n_theta)
 phi = np.linspace(0, 2*np.pi, n_phi)
 ttheta, pphi = np.meshgrid(theta, phi, indexing = 'ij')
 struct.clip(mi,ma, out=struct)
+fig = plt.figure()
+
+# Plot cartesian projection
+#plt.imshow(struct.T, cmap=cmap, norm='log')
+#plt.xticks([])
+#plt.yticks([])
+#plt.colorbar()
 
 # HealPy transformation
 nside = 100
@@ -43,8 +57,8 @@ m[pixel_indices] = struct
 m.clip(mi, ma, out=m)
 
 # Plot as Mollweide projection
-fig = plt.figure()
-hp.mollview(m, fig=fig, norm='log', title='',  rot=(180, 0, 0), flip='geo')
+#hp.mollview(m, fig=fig, norm='log', title='',  rot=(180, 0, 0), flip='geo', cmap=cmap)
+hp.mollview(m, fig=fig, min=mi, max=ma, norm='log', title='',  rot=(180, 0, 0), flip='geo', cmap=cmap)
 
 # Make colorbar better
 cb = fig.get_axes()[1]
@@ -53,4 +67,5 @@ cb.set_xticks(ticks)
 logticks = ['%2.1f' % tick for tick in np.log10(ticks)]
 cb.set_xticklabels(logticks)
 cb.text( 0.5, -4.0, cbtext, transform=cb.transAxes, ha="center", va="bottom")
+plt.tight_layout()
 plt.savefig('%s.png' % name)
